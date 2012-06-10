901c7f989b265c6bab32e96b3d2f48654189cb2d
Maxious <maxious@lambdacomplex.org>
2012-06-10 12:04:14 +1000

    Add route_url for routes.txt

--- routes.php
+++ routes.php
@@ -1,6 +1,46 @@
 <?php
 
 include 'common.inc.php';
+
+function buildRouteURL($routeNum) {
+    $specialRoutes = Array(
+        11 => "11_111",
+        12 => "12_312",
+        13 => "13_313",
+        14 => "14_314",
+        15 => "15_315",
+        18 => "18_318",
+        19 => "19_319",
+        25 => "25_225",
+        26 => "26_226",
+        27 => "27_227",
+        60 => "60_160",
+        61 => "61_161",
+        62 => "62_162",
+        65 => "65_265",
+        67 => "67_267",
+        111 => "11_111",
+        312 => "12_312",
+        313 => "13_313",
+        314 => "14_314",
+        315 => "15_315",
+        318 => "18_318",
+        319 => "19_319",
+        225 => "25_225",
+        226 => "26_226",
+        227 => "27_227",
+        160 => "60_160",
+        161 => "61_161",
+        162 => "62_162",
+        265 => "65_265",
+        267 => "67_267",
+    );
+    if (array_key_exists($routeNum, $specialRoutes)) {
+        $routeNum = $specialRoutes[$routeNum];
+    }
+    return "https://www.action.act.gov.au/routes/" . $routeNum . ".htm";
+}
+
 $file = "routes.txt";
 $debug = false;
 $line = 0;
@@ -28,6 +68,7 @@ if ($inhandle && $outhandle) {
                     }
                     echo "---\n";
                 }
+                $data[array_search("route_url", $headers)] = buildRouteURL($data[array_search("route_id", $headers)]);
                 $data[array_search("route_long_name", $headers)] = cleanStopName($data[array_search("route_long_name", $headers)]);
                 $data[array_search("route_text_color", $headers)] = "000000";
                 $data[array_search("route_color", $headers)] = "FFFFFF"; #default  black on white
