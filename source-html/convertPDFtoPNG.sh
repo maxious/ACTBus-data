e4082c4b1aee7d5441cd02ad66c2af6ef523ff60
Maxious <maxious@lambdacomplex.org>
2012-07-09 19:30:05 +1000

    June 2012 update inc. depreciate all html scraping

--- source-html/convertPDFtoPNG.sh
+++
@@ -1,2 +0,0 @@
-gs -q -sDEVICE=pngalpha -dBATCH -dNOPAUSE -dFirstPage=1 -dLastPage=1 -r400 -sOutputFile=weekday_bus_map.png weekday_bus_map.pdf
-gs -q -sDEVICE=pngalpha -dBATCH -dNOPAUSE -dFirstPage=1 -dLastPage=1 -r400 -sOutputFile=weekend_bus_map.png weekend_bus_map.pdf
--- source-html/get.sh
+++
@@ -1,3 +0,0 @@
-wget -nd -np -r --level=1 --random-wait -c http://www.action.act.gov.au/routes_by_number.html
-grep "Page not found" *
-
