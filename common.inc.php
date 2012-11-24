2ceec33571b0d5d07ce96fd7bed57c045a5ff9c9
Maxious <maxious@lambdacomplex.org>
2012-11-24 23:50:35 +1100

    nov 2012 updates

--- common.inc.php
+++ common.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 function getPage($url) {
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -8,40 +9,59 @@ function getPage($url) {
     return $page;
 }
 
-function cleanServiceID($serviceID){
-    
-    
-    if (strpos($serviceID, "Vac-Weekday")) return "Weekday-SchoolVacation";
-        if (strpos($serviceID, "SAT-Saturday")) return "Saturday";
-        if (strpos($serviceID, "SUN-Sunday")) return "Sunday";
-        if (strpos($serviceID, "MAST-Saturday")) return "Saturday";
-        if (strpos($serviceID, "MAST-Sunday")) return "Sunday";
-        if (strpos($serviceID, "xmas2011")) return "Christmas2011";
-        if (strpos($serviceID, "MAST-Weekday")) return "Weekday";
-        if (strpos($serviceID, "3DXS-Weekday")) return "Weekday-EndOfYearHolidays";
-        die("Unknown service ID $serviceID");
+function cleanServiceID($serviceID) {
+
+
+    /* if (strpos($serviceID, "Vac-Weekday"))
+      return "Weekday-SchoolVacation";
+      if (strpos($serviceID, "MAST-Weekday"))
+      return "Weekday";
+      if (strpos($serviceID, "MAST-Saturday"))
+      return "Saturday";
+      if (strpos($serviceID, "MAST-Sunday"))
+      return "Sunday";
+     */
+    $mapping = Array(
+        "2012-COMBMAST-Saturday-04" => "Saturday12.0",
+        "2012-COMBMAST-Saturday-05" => "Saturday12.1",
+        "2013-COMBMAST-Saturday-01" => "Saturday13.0",
+        
+        "2012-COMBMAST-Sunday-05" => "Sunday12.0",
+        "2012-xmas2012-Sunday-00" => "Xmas-Sunday",
+        "2013-COMBMAST-Sunday-01" => "Sunday13.0",
+        
+        "2012-COMBMAST-Weekday-14" => "Weekday12.0",
+        "2012-COMBMAST-Weekday-12" => "Weekday12.1",
+        "2012-MODXMAS-Weekday-01"  => "Xmas-Weekday12.1",
+        "2013-MODXMAS-Weekday-01"  => "Xmas-Weekday13.0",
+        "2013-COMBSVac-Weekday-01" => "Weekday13.0"
+    );
+    return $mapping[$serviceID];
+
+    die("Unknown service ID $serviceID");
 }
 
 function cleanRouteID($routeID) {
-    $routeParts = explode("-",$routeID);
+    $routeParts = explode("-", $routeID);
     return $routeParts[0];
 }
 
 function cleanStopName($name) {
-    $name = str_replace(Array("from","before","after","opp","Service","Platform"),"",$name);
-    $name = str_replace("Pk","Park",$name);
-    $name = str_replace("Dr ","Drive ",$name);    
-    $name = str_replace("Drive","Drive ",$name);
-    $name = str_replace("Rd","Road",$name);
-    $name = str_replace("Cr","Crescent",$name);
-    $name = str_replace("Cct ","Circuit ",$name);
-    $name = str_replace("Circuit","Circuit ",$name);
-    $name = str_replace("Av ","Avenue ",$name);
-    $name = str_replace("Avenue","Avenue ",$name);
-    $name = str_replace("Bus","Bus Station",$name);
-    $name = str_replace("Station Station","Station",$name);
-    $name = str_replace("Station Station","Station",$name);
-    $name = str_replace("  "," ",$name);
+    $name = str_replace(Array("from", "before", "after", "opp", "Service", "Platform"), "", $name);
+    $name = str_replace("Pk", "Park", $name);
+    $name = str_replace("Dr ", "Drive ", $name);
+    $name = str_replace("Drive", "Drive ", $name);
+    $name = str_replace("Rd", "Road", $name);
+    $name = str_replace("Cr", "Crescent", $name);
+    $name = str_replace("Cct ", "Circuit ", $name);
+    $name = str_replace("Circuit", "Circuit ", $name);
+    $name = str_replace("Av ", "Avenue ", $name);
+    $name = str_replace("Avenue", "Avenue ", $name);
+    $name = str_replace("Bus", "Bus Station", $name);
+    $name = str_replace("Station Station", "Station", $name);
+    $name = str_replace("Station Station", "Station", $name);
+    $name = str_replace("  ", " ", $name);
     return trim($name);
 }
+
 ?>
